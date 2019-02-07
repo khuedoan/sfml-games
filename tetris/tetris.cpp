f148678790dca69df68689cbad53df6879b5fc4b
Khue Doan <khuedoan98@gmail.com>
2019-02-07 13:33:42 +0700

    Add window and texture handling

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -2,5 +2,32 @@
 
 int main()
 {
+	sf::RenderWindow mainWindow(sf::VideoMode(320, 640),
+								"Tetris",
+								sf::Style::Close | sf::Style::Titlebar);
+	
+	sf::Texture blocksTexture;
+	blocksTexture.loadFromFile("textures/blocks.png");
+
+	sf::Sprite blocksSprite(blocksTexture);
+	blocksSprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+
+	while (mainWindow.isOpen())
+	{
+		sf::Event windowEvent;
+
+		while (mainWindow.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == windowEvent.Closed)
+			{
+				mainWindow.close();
+			}
+		}
+
+		mainWindow.draw(blocksSprite);
+		mainWindow.display();
+	}
+
+
 	return 0;
 }
